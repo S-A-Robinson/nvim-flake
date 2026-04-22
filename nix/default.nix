4d2d89aa50560ff9aa6ec47bb7dda8f762b60b86
S-A-Robinson <scott@motion12.digital>
2026-04-22 09:55:16 +0100

    feat: add gitsigns to categories

--- nix/default.nix
+++ nix/default.nix
@@ -381,6 +381,7 @@ let
           flash = true;
           fzf-lua = true;
           github = true;
+          gitsigns = true;
           glance = true;
           grug-far = true;
           hydra = true;
