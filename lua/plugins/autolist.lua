a73a637d03053d210cd7385b1028cb64db4d8021
S-A-Robinson <scott@motion12.digital>
2026-08-14 14:03:30 +0100

    fix: use after instead of config for autolist.nvim

--- lua/plugins/autolist.lua
+++ lua/plugins/autolist.lua
@@ -1,35 +1,33 @@
 return {
-	{
-		"autolist.nvim",
-		for_cat = "autolist",
-		ft = {
-			"markdown",
-			"text",
-			"tex",
-			"plaintex",
-			"norg",
-		},
-		config = function()
-			require("autolist").setup()
-
-			vim.keymap.set("i", "<tab>", "<cmd>AutolistTab<cr>")
-			vim.keymap.set("i", "<s-tab>", "<cmd>AutolistShiftTab<cr>")
-
-			vim.keymap.set("i", "<CR>", "<CR><cmd>AutolistNewBullet<cr>")
-			vim.keymap.set("n", "o", "o<cmd>AutolistNewBullet<cr>")
-			vim.keymap.set("n", "O", "O<cmd>AutolistNewBulletBefore<cr>")
-			vim.keymap.set("n", "<CR>", "<cmd>AutolistToggleCheckbox<cr><CR>")
-			vim.keymap.set("n", "<C-r>", "<cmd>AutolistRecalculate<cr>")
-
-			-- cycle list types with dot-repeat
-			vim.keymap.set("n", "<leader>cn", require("autolist").cycle_next_dr, { expr = true })
-			vim.keymap.set("n", "<leader>cp", require("autolist").cycle_prev_dr, { expr = true })
-
-			-- functions to recalculate list on edit
-			vim.keymap.set("n", ">>", ">><cmd>AutolistRecalculate<cr>")
-			vim.keymap.set("n", "<<", "<<<cmd>AutolistRecalculate<cr>")
-			vim.keymap.set("n", "dd", "dd<cmd>AutolistRecalculate<cr>")
-			vim.keymap.set("v", "d", "d<cmd>AutolistRecalculate<cr>")
-		end,
+	"autolist.nvim",
+	for_cat = "autolist",
+	ft = {
+		"markdown",
+		"text",
+		"tex",
+		"plaintex",
+		"norg",
 	},
+	after = function()
+		require("autolist").setup()
+
+		vim.keymap.set("i", "<tab>", "<cmd>AutolistTab<cr>")
+		vim.keymap.set("i", "<s-tab>", "<cmd>AutolistShiftTab<cr>")
+
+		vim.keymap.set("i", "<CR>", "<CR><cmd>AutolistNewBullet<cr>")
+		vim.keymap.set("n", "o", "o<cmd>AutolistNewBullet<cr>")
+		vim.keymap.set("n", "O", "O<cmd>AutolistNewBulletBefore<cr>")
+		vim.keymap.set("n", "<CR>", "<cmd>AutolistToggleCheckbox<cr><CR>")
+		vim.keymap.set("n", "<C-r>", "<cmd>AutolistRecalculate<cr>")
+
+		-- cycle list types with dot-repeat
+		vim.keymap.set("n", "<leader>cn", require("autolist").cycle_next_dr, { expr = true })
+		vim.keymap.set("n", "<leader>cp", require("autolist").cycle_prev_dr, { expr = true })
+
+		-- functions to recalculate list on edit
+		vim.keymap.set("n", ">>", ">><cmd>AutolistRecalculate<cr>")
+		vim.keymap.set("n", "<<", "<<<cmd>AutolistRecalculate<cr>")
+		vim.keymap.set("n", "dd", "dd<cmd>AutolistRecalculate<cr>")
+		vim.keymap.set("v", "d", "d<cmd>AutolistRecalculate<cr>")
+	end,
 }
